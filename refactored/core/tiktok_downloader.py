df18c3e984639c4da950c6764553318e85b9e7a0
ne0ascorbinka <ne.ascorbinka@gmail.com>
2023-08-25 22:23:57 +0300

    + tt

--- refactored/core/tiktok_downloader.py
+++ refactored/core/tiktok_downloader.py
@@ -16,40 +16,36 @@ class TiktokDownloader(BaseDownloader):
         link = re.findall(r'\bhttps?://.*[(tiktok|douyin)]\S+', self.url)[0]
         link = link.split("?")[0]
 
-        params = {
-            "link": link
-        }
+        url = "https://tiktok82.p.rapidapi.com/getDownloadVideoWithoutWatermark"
+
+        querystring = {"video_url" : link}
+
         headers = {
-            'x-rapidapi-host': "tiktok-info.p.rapidapi.com",
-            'x-rapidapi-key': "685f6e97c3mshe1b81cdc5fd9189p1975ecjsna5d45b82df8a"
+            "X-RapidAPI-Key": "685f6e97c3mshe1b81cdc5fd9189p1975ecjsna5d45b82df8a",
+            "X-RapidAPI-Host": "tiktok82.p.rapidapi.com"
         }
 
-        # Get your Free TikTok API from https://rapidapi.com/TerminalWarlord/api/tiktok-info/
-        # Using the default one can stop working any moment
+        response = requests.get(url, headers=headers, params=querystring)
 
-        api = f"https://tiktok-info.p.rapidapi.com/dl/"
-        print(link)
-        r = requests.get(api, params=params, headers=headers).json()
-        print("here goes r")
-        print(r)
-        r = r['videoLinks']['download']
+        # print(response.json())
+        download_url = response.json()["video_url"]
+
         directory = self.folder_path
         filename = str(int(time.time())) + '.mp4'
         content_path = os.path.join(".", directory, filename)
         self.content_path = content_path
-        
             
-        with requests.get(r, timeout=(50, 10000), stream=True) as r:
-            r.raise_for_status()
+        with requests.get(download_url, timeout=(50, 10000), stream=True) as response:
+            response.raise_for_status()
             with open(content_path, 'wb') as f:
                 chunk_size = 1048576
-                for chunk in r.iter_content(chunk_size=chunk_size):
+                for chunk in response.iter_content(chunk_size=chunk_size):
                     f.write(chunk)
     
     def _upload(self) -> None:
         sent_message = self.sent_message
         sent_message.edit(f"__Downloaded! Uploading...__")
-        self.client.send_video(self.id, self.content_path)
+        self.client.send_video(self.id, self.content_path, caption=f"__via @{config.BOT_USERNAME}__")
         sent_message.delete()
         
 
